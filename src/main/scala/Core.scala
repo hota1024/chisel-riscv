9844b21b3da45a960407c696a43c6c357c52ff22
= <hotalog@hota1024.com>
2023-04-05 08:38:53 +0000

    ♻️デコーダを良い感じに変更

--- src/main/scala/Core.scala
+++ src/main/scala/Core.scala
@@ -51,6 +51,69 @@ class Core extends Module {
   val imm_s = Cat(inst(31, 25), inst(11, 7))
   val imm_s_sext = Cat(Fill(20, imm_s(11)), imm_s)
 
+  val csignals = ListLookup(
+    inst,
+    List(          ALU_X,   OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X),
+    Array(
+      LW   -> List(ALU_ADD, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_MEM), // sext(M[x[rs1] + sext(offset)][31:0])
+      SW   -> List(ALU_ADD, OP1_RS1, OP2_IMS, MEN_S, REN_X, WB_X  ), // M[x[rs1] + sext(offset)] = x[rs2][31:0]
+
+      ADD  -> List(ALU_ADD, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU), // x[rs1] + x[rs2]
+      ADDI -> List(ALU_ADD, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU), // x[rs1] + sext(immediate)
+      SUB  -> List(ALU_SUB, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU), // x[rs1] - x[rs2]
+
+      AND  -> List(ALU_AND, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU), // x[rs1] & x[rs2]
+      OR   -> List(ALU_OR,  OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU), // x[rs1] | x[rs2]
+      XOR  -> List(ALU_XOR, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU), // x[rs1] ^ x[rs2]
+
+      ANDI  -> List(ALU_AND, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU), // x[rs1] & sext(immediate)
+      ORI   -> List(ALU_OR,  OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU), // x[rs1] | sext(immediate)
+      XORI  -> List(ALU_XOR, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU), // x[rs1] ^ sext(immediate)
+    )
+  )
+
+  /**
+    * exe_fun : 使用するALU命令
+    * ├─ src/main/scala/common/Consts.scala を参照
+    * 
+    * op1_sel : 第1オペランド
+    * ├─ OP1_RS1 : rs1_data
+    * 
+    * op2_sel : 第2オペランド
+    * ├─ OP2_RS2 : rs2_data
+    * ├─ OP2_IMI : imm_i_sext
+    * ├─ OP2_IMS : imm_s_sext
+    * 
+    * mem_wen : 書き込みの有無
+    * ├─ MEN_X : 書き込み無し
+    * ├─ MEN_S : メモリへの書き込み有り
+    * 
+    * rf_wen : Write-Back の有無
+    * ├─ REN_X : Write-Back 無し
+    * ├─ REN_S : メモリへの Write-Back 有り
+    * 
+    * wb_sel : Write-Back するデータソース
+    * ├─ WB_X   : データ無し
+    * ├─ WB_ALU : 演算結果(alu_out)
+    * ├─ WB_MEM : メモリ(io.dmem.rdata)
+    */
+  val exe_fun :: op1_sel :: op2_sel :: mem_wen ::rf_wen :: wb_sel :: Nil = csignals
+
+  val op1_data = MuxCase(
+    0.U(WORD_LEN.W),
+    Seq(
+      (op1_sel === OP1_RS1) -> rs1_data,
+    )
+  )
+  val op2_data = MuxCase(
+    0.U(WORD_LEN.W),
+    Seq(
+      (op2_sel === OP2_RS2) -> rs2_data,
+      (op2_sel === OP2_IMI) -> imm_i_sext,
+      (op2_sel === OP2_IMS) -> imm_s_sext,
+    )
+  )
+
   /**
     * RISC-V では0番レジスタの値は常に0になる。
     */
@@ -66,20 +129,13 @@ class Core extends Module {
 
   // 演算した結果を alu_out に接続
   val alu_out = MuxCase(
-    0.U(WORD_LEN), // デフォルト値
+    0.U(WORD_LEN),
     Seq(
-      (inst === LW || inst === ADDI) -> (rs1_data + imm_i_sext), // I命令の出力先アドレスを演算
-      (inst === SW)                  -> (rs1_data + imm_s_sext), // Store先アドレスを演算
-      (inst === ADD)                 -> (rs1_data + rs2_data),   // x[rs1] + x[rs2]
-      (inst === SUB)                 -> (rs1_data - rs2_data),   // x[rs1] - x[rs2]
-
-      (inst === AND) -> (rs1_data & rs2_data), // x[rs1] & x[rs2]
-      (inst === OR)  -> (rs1_data | rs2_data), // x[rs1] | x[rs2]
-      (inst === XOR) -> (rs1_data ^ rs2_data), // x[rs1] ^ x[rs2]
-
-      (inst === ANDI) -> (rs1_data & imm_i_sext), // x[rs1] & sext(imm_i)
-      (inst === ORI)  -> (rs1_data | imm_i_sext), // x[rs1] | sext(imm_i)
-      (inst === XORI) -> (rs1_data ^ imm_i_sext), // x[rs1] ^ sext(imm_i)
+      (exe_fun === ALU_ADD) -> (op1_data + op2_data),
+      (exe_fun === ALU_SUB) -> (op1_data - op2_data),
+      (exe_fun === ALU_AND) -> (op1_data & op2_data),
+      (exe_fun === ALU_OR)  -> (op1_data | op2_data),
+      (exe_fun === ALU_XOR) -> (op1_data ^ op2_data),
     )
   )
 
@@ -88,38 +144,21 @@ class Core extends Module {
 
   io.dmem.addr := alu_out // 算出したメモリアドレスをデータ用メモリのアドレスに接続
 
-  io.dmem.wen   := (inst === SW) // SW 命令なら書き込み信号をON
+  io.dmem.wen   := mem_wen
   io.dmem.wdata := rs2_data      // 書き込むデータを接続
 
   /*-------------------------*/
   /* [WB - Write-Back Stage] */
 
-  // Write-Back するデータ
   val wb_data = MuxCase(
-    alu_out, // デフォルトでは alu_out を選択
+    alu_out, // WB_ALU が定義されているが alu_out はデフォルトの接続元にしている。
     Seq(
-      (inst === LW) -> io.dmem.rdata, // LW ならデータを読み込む
+      (wb_sel === WB_MEM) -> io.dmem.rdata,
     )
   )
 
-  when(
-       inst === LW
-    // 加減演算
-    || inst === ADD
-    || inst === ADDI
-    || inst === SUB
-
-    // 論理演算(R形式)
-    || inst === AND
-    || inst === OR
-    || inst === XOR
-
-    // 論理演算(I形式)
-    || inst === ANDI
-    || inst === ORI
-    || inst === XORI
-  ) {
-    // レジスタ wb_data を書き込む
+  when(rf_wen === REN_S) {
+    // wb_addr が示すレジスタに Write-Back する。
     regfile(wb_addr) := wb_data
   }
 
